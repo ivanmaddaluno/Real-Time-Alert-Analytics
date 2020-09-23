043a58fffe2c0bd34b80cd7cf949c65ae6d75c50
Ivan Maddaluno <imaddaluno@bern0139.gs24.com>
2020-09-23 13:42:57 +0200

    Alpha Version

---
+++ routes/sendToMe.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const sendToMeRouter = express.Router();
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+console.log("from Router");
+
+const transport = {
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.THE_EMAIL,
+        pass: process.env.THE_PASSWORD,
+    },
+};
+
+const transporter = nodemailer.createTransport(transport);
+transporter.verify((error, success) => {
+    if (error) {
+        console.error(error);
+    } else {
+        //Success
+        console.log("users ready to mail myself");
+    }
+});
+
+sendToMeRouter.post("/", (req, res, next) => {
+    //make mailable object
+    const mail = {
+        from: process.env.THE_EMAIL,
+        to: "ivan.maddaluno@scout24.com",
+        subject: req.body.subject,
+        text: `
+      from:
+      ${req.body.name} 
+
+      contact: ${req.body.email}
+
+      message: 
+
+      ${req.body.text}`,
+    };
+    transporter.sendMail(mail, (err, data) => {
+        if (err) {
+            res.json({
+                status: "fail",
+            });
+        } else {
+            res.json({
+                status: "success",
+            });
+        }
+    });
+});
+
+module.exports = sendToMeRouter;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const sendToMe = require("./routes/sendToMe");
+require("dotenv").config();
+const path = require("path");
+const cors = require("cors");
+
+const morgan = require("morgan");
+const PORT = 4200;
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/rtAnalytics.js", (req, res) => {
+    res.sendFile(path.join(__dirname + "/rtAnalytics.js"));
+});
+
+app.use("/sendToMe", sendToMe);
+
+app.listen(PORT, () => {
+    console.log("App is live");
+});
